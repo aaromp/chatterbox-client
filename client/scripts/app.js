91cfbed8442211c7cb6ce53af8268a590129c252
Hack Reactor Students <aaromp@gmail.com>
2014-04-28 15:33:45 -0700

    Added simple app behavior

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,53 @@
 // YOUR CODE HERE:
 
+var app = {
+  init: function(){
+
+  },
+  send: function(message){
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+
+  fetch: function(){
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+       // data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message received');
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to receive message');
+      }
+    });
+  },
+
+  clearMessages: function() {
+    $('#chats').fadeOut();
+  },
+
+  addMessage: function(message) {
+    $('#main').append('<div id="chats"></div>');//.attr('id', 'chats');
+    $('#chats').html('<p>'+message.message+'</p>');
+  },
+
+  addRoom: function(room){
+    $('#roomSelect').append('<option>' + room + '</option>');
+  }
+};
+
+$(document).ready(function(){
+  $('.clearButton').click(app.clearMessages);
+});
