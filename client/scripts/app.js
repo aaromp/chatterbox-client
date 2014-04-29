20011afade8a771885436b5455a98ac1a93d433d
Hack Reactor Students <aaromp@gmail.com>
2014-04-28 19:35:39 -0700

    add posting and fetching

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,6 +3,7 @@
 var app = {
   init: function(){
 
+
   },
   send: function(message){
     $.ajax({
@@ -24,11 +25,14 @@ var app = {
       //async: false,
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'GET',
-       // data: JSON.stringify(message),
+      data: {
+        order: '-createdAt',
+        limit: maxNumPosts
+      },
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message received');
-        renderMessages(data.results);
+        app.renderPage(data.results);
 
         return data.results;
       },
@@ -42,33 +46,69 @@ var app = {
     $('#chats').children().remove();
   },
 
-  addMessage: function(message) {
-    // $('#main').append('<div id="chats"></div>');//.attr('id', 'chats');
-    if (typeof message.text === 'object') console.log(message.text);
-    if (message.text !== undefined) message = message.text.replace(/[<>]|document.[^ ]/g,'');
-    // [<>]|document.[^ ]
-    $('#chats').append('<p>'+message+'</p>');
+  addMessage: function(data) {
+
+    var user = data.username;
+    var ts = data.updatedAt;
+    //console.log(ts);
+    var message = data.text;
+    var room = data.roomname;
+
+    if (message) {
+      message = message.replace(/[<>]|document.[^ ]|\$.[^ ]|\$\(/g,'');
+    }
+    // [<>]|document.[^ ]|\$.[^ ]|\$\(
+    $('#chats').append('<p class="' + user + '">' + user + ' : ' + message + '</p>');
   },
 
   addRoom: function(room){
     $('#roomSelect').append('<option>' + room + '</option>');
+  },
+
+  renderPage: function(data) {
+    app.clearMessages();
+
+    for (var i = 0; i < data.length; i++) {
+      app.addMessage(data[i]);
+    }
+
+    var rooms = {};
+    for (var j = 0; j < data.length; j++) {
+      rooms[data[j].roomname] = true;
+    }
+
+    for (var room in rooms) {
+      app.addRoom(room);
+    }
   }
 };
 
 /* run code */
 
-var renderMessages = function(data) {
-  app.clearMessages();
-  for (var i = 0; i < data.length; i++) {
-    app.addMessage(data[i]);
-  }
-};
 
+
+var query = window.location.search;
+var username = query.slice(query.lastIndexOf('=') + 1);
+var maxNumPosts = 25;
+var refreshLength = 2000;
 
 $(document).ready(function(){
   $('.clearButton').click(app.clearMessages);
+  $('.input').keypress(function(e) {
 
-  setInterval(function(){
+    if(e.which === 13) {
+      var message = {
+        username: username,
+        text: $(this).val(),
+        roomname: ""
+      };
+      console.log(message);
+      app.send(message);
+      $(this).val('');
+    }
+  });
+
+  //setInterval(function(){
     app.fetch();
-  }, 2000);
+  //}, refreshLength);
 });
